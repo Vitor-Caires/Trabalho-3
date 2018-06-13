57b0b19960f7ce94cc5a955483ca6c59bf361fec
Vitor Caires <vitorcaires@usp.br>
2018-06-13 12:57:53 -0300

    Initial Commit

---
+++ Trabalho 3/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Trabalho 3
+//
+//  Created by Vitor Caires on 13/06/18.
+//  Copyright © 2018 Vitor Caires. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
